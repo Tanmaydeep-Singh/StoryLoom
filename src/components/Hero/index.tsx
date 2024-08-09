9f7dcb1325217058e8f4220ab9b50cd36874a3ec
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-08-10 02:00:22 +0530

    dev: bg updated

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div
       className="relative top-0 flex items-center justify-center w-full h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url('/bg0temp.jpeg')` }}
+      style={{ backgroundImage: `url('/Frame.png')` }}
     >
       <div className="relative z-10 flex flex-col items-center justify-center w-[90vw] h-[60vh] text-center text-white px-4">
         <TextCycle />
