79db3be65719a7d4d2de348e7eab3409328a64bc
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-08-13 13:36:37 +0530

    dev: gradient added

--- src/components/AnimationSection/YTranslateCubic/index.tsx
+++ src/components/AnimationSection/YTranslateCubic/index.tsx
@@ -6,7 +6,7 @@ const YTranslateCubic = ({ children }: any) => {
     const isInView = useInView(ref, { once: true });
 
     return (
-        <section ref={ref} className=" my-10 md:mt-20" style={{
+        <section ref={ref} className="bg-gradient-to-b from-[#010509]  via-transparent to-transparent  " style={{
             transform: isInView ? "none" : "translateY(100px)",
             opacity: isInView ? 1 : 0,
             transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
--- src/components/HighlightSection/index.tsx
+++ src/components/HighlightSection/index.tsx
@@ -2,14 +2,9 @@ import Image from 'next/image';
 
 const HighlightSection = () => {
   return (
-    <div className="relative w-[97vw] h-[100vh] flex flex-col items-center justify-center bg-cover bg-center mx-auto my-8" style={{ backgroundImage: "url('/highlight.png')" }}>
+    <div className="relative w-[97vw] mt-24 h-[100vh] flex flex-col items-center justify-center bg-cover bg-center mx-auto mb-8" style={{ backgroundImage: "url('/highlight.png')" }}>
       {/* Fade effect for top and bottom */}
-      <div className="absolute inset-0">
-        {/* Fade effect at the top */}
-        <div className="absolute inset-0 bg-gradient-t-to-b from-black via-transparent to-transparent opacity-40"></div>
-        {/* Fade effect at the bottom */}
-        <div className="absolute inset-0  bg-gradient-b-to-t from-black via-transparent to-transparent opacity-40"></div>
-      </div>
+      
       
       {/* Centered text */}
       <div className="relative z-10 flex items-center justify-center h-full px-4 sm:px-8">
