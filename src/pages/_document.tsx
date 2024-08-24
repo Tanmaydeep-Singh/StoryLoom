1383c237bb8cca583b16316eaa497ed66df13f19
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-08-24 16:50:03 +0530

    dev: metadata update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,13 +18,13 @@ export default function Document() {
         <meta name="twitter:site" content="@storyloom" />
         <meta name="twitter:title" content="Storyloom - Explore and Contribute to Global Folktales" />
         <meta name="twitter:description" content="Discover and share folktales from various cultures with Storyloom. Join a community of readers and writers to explore the world through stories." />
-        <meta name="twitter:image" content="/logo/logo.png" />
+        <meta name="twitter:image" content="/Logos/logo.png" />
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/Logos/favicon.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/Logos/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#000000" />
 
